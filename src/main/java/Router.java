eea14ab39ae5cb550b72dd4556e0326bec4f783b
nolanpollack <nolanfpollack@gmail.com>
2023-02-09 17:15:58 -0500

    Added functionality to send

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -6,8 +6,14 @@ import java.lang.reflect.Type;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Router {
@@ -15,7 +21,7 @@ public class Router {
     static Map<String, DatagramSocket> sockets = new HashMap<>();
     static Map<String, Integer> ports = new HashMap<>();
     private final int asn;
-
+    Selector selector;
     private Gson gson;
 
     public Router(int asn, String[] connections) throws Exception {
@@ -23,13 +29,21 @@ public class Router {
 
         System.out.println("Router at AS " + asn + " starting up");
         this.asn = asn;
+        selector = Selector.open();
+
         for (String relationship : connections) {
             String[] parts = relationship.split("-");
             String port = parts[0];
             String neighbor = parts[1];
             String relation = parts[2];
 
-            sockets.put(neighbor, new DatagramSocket(Integer.parseInt(port), InetAddress.getLocalHost()));
+            //Register the socket with the selector
+            DatagramSocket socket;
+            DatagramChannel channel = DatagramChannel.open();
+            socket = channel.socket();
+            socket.bind(new InetSocketAddress(InetAddress.getLocalHost(), Integer.parseInt(port)));
+
+            sockets.put(neighbor, socket);
             ports.put(neighbor, Integer.parseInt(port));
             relations.put(neighbor, relation);
 
@@ -52,30 +66,41 @@ public class Router {
 
     public void send(String network, String message) throws Exception {
         System.out.println("Sending message '" + message + "' to " + network);
-        DatagramPacket dp = new DatagramPacket(message.getBytes(), message.length(), InetAddress.getLocalHost(), ports.get(network));
-        sockets.get(network).send(dp);
+        DatagramPacket dp = new DatagramPacket(message.getBytes(), message.length(), InetAddress.getByName(network), ports.get(network));
+
+        DatagramSocket socket = sockets.get(network);
+        socket.send(dp);
     }
 
     public void run() throws Exception {
-//        while (true) {
-//            List<DatagramSocket> socketList = new ArrayList<>(sockets.values());
-//            int index = DatagramSocket.getSelector().select(100);
-//            if (index > 0) {
-//                DatagramSocket conn = socketList.get(index - 1);
-//                byte[] buffer = new byte[65535];
-//                DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
-//                conn.receive(dp);
-//                String srcif = null;
-//                for (Map.Entry<String, DatagramSocket> entry : sockets.entrySet()) {
-//                    if (entry.getValue() == conn) {
-//                        srcif = entry.getKey();
-//                        break;
-//                    }
-//                }
-//                String msg = new String(dp.getData(), 0, dp.getLength());
-//                System.out.println("Received message '" + msg + "' from " + srcif);
-//            }
-//        }
+        Selector selector = Selector.open();
+        for (DatagramSocket socket : sockets.values()) {
+            socket.getChannel().configureBlocking(false);
+            socket.getChannel().register(selector, SelectionKey.OP_READ);
+        }
+
+        while (true) {
+            int readyChannels = selector.select();
+            if (readyChannels == 0) {
+                continue;
+            }
+            Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
+
+            while (keys.hasNext()) {
+                SelectionKey key = keys.next();
+                if (key.isReadable()) {
+                    // Read the incoming data
+                    ByteBuffer buffer = ByteBuffer.allocate(65535);
+                    DatagramChannel channel = (DatagramChannel) key.channel();
+                    InetSocketAddress source = (InetSocketAddress) channel.receive(buffer);
+
+                    buffer.flip();
+                    String msg = new String(buffer.array(), 0, buffer.limit());
+                    System.out.println("Received message '" + msg + "' from " + source);
+                }
+                keys.remove();
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -88,7 +113,7 @@ public class Router {
         String[] connections = Arrays.copyOfRange(args, 1, args.length);
 
         Router router = new Router(asn, connections);
-//        router.run();
+        router.run();
     }
 
     class MessageSerializer implements JsonSerializer<Message> {
